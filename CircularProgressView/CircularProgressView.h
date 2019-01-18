3bcb363ce2f1805b1da89758bf28d78e9d3a5da8
J.Vignesh <jvigneshcs@gmail.com>
2019-01-18 13:16:07 +0530

    Added shared framework to support Carthage

---
+++ CircularProgressView/CircularProgressView.h
@@ -0,0 +1,19 @@
+//
+//  CircularProgressView.h
+//  CircularProgressView
+//
+//  Created by Vignesh J on 18/01/19.
+//  Copyright © 2019 Vignesh J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircularProgressView.
+FOUNDATION_EXPORT double CircularProgressViewVersionNumber;
+
+//! Project version string for CircularProgressView.
+FOUNDATION_EXPORT const unsigned char CircularProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircularProgressView/PublicHeader.h>
+
+
